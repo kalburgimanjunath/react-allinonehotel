c22443aca73c0d96ef06b552af0adfada028dc3e
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-01-25 12:57:53 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-import { Header, Footer, Content } from 'components/index';
+import { Header, Footer, Content } from './components/index';
 export default function App() {
   return (
     <div>
