2c2a9033009465158c308630ca5906e34365950e
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-01-25 13:29:09 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './style.css';
 import { Header, Footer, Content } from './components/index';
+import {
+  Courses,
+  Dashboard,
+  Doctors,
+  Home,
+  Hospitals,
+  Login,
+  News,
+  Signup,
+} from './pages/index';
 export default function App() {
   return (
     <div>
---
+++ src/Pages/index.js
@@ -0,0 +1,9 @@
+import Courses from './Courses';
+import Dashboard from './Dashboard';
+import Doctors from './Doctors';
+import Home from './Home';
+import Hospitals from './Hospitals';
+import Login from './Login';
+import News from './News';
+import Signup from './Signup';
+export { Courses, Dashboard, Doctors, Home, Hospitals, Login, News, Signup };
