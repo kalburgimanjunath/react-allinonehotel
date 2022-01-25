afb1669cbe1bab1ca2659f7e73abff4db9075a88
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-01-25 13:01:45 +0530

    7 files modified

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from './Header';
+import Footer from './Footer';
+import Content from './Content';
+import Posts from './Posts';
+import Sidebar from './Sidebar';
+import Widget from './Widget';
+export {Header,Footer,Sidebar,Content,Widget,Posts}
