7394939f9b54a0e8f33826b3cca0099b32e67134
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-01-25 12:54:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-b2f9vn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-b2f9vn)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+import { Header, Footer, Content } from 'components/index';
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
