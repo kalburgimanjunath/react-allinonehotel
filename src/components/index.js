983b0263d7565a60bcdc51dc45c41f820d231bc3
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-01-25 13:04:09 +0530

    1 file modified, 6 added

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ import Content from './Content';
 import Posts from './Posts';
 import Sidebar from './Sidebar';
 import Widget from './Widget';
-export {Header,Footer,Sidebar,Content,Widget,Posts}
+export { Header, Footer, Sidebar, Content, Widget, Posts };
